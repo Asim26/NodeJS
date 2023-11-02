7d8292fb3ef7731ddd30f247fa5095759e724691
asim_mehmood_11 <asim.mehmood@codedistrict.com>
2023-11-02 18:34:00 +0500

    code refactored

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,13 @@
-const express = require('express')
+const express = require('express');
+const { courses } = require('../utils');
 const router = express.Router();
 
 router.get('/', (req, rsp) => {
-    rsp.render('index', {title:'My Express App', message:'List of Courses'})
+    rsp.render('index', {
+        title: 'My Express App',
+        message: 'List of Courses',
+        courses: courses
+    })
 });
 
 module.exports = router;
